eee360bd6901f1f41ff6f57df867cf7857bef15a
MusicalMoon <sakinam831@gmail.com>
2023-12-04 21:30:00 -0600

    test

--- server/config/rolesList.js
+++ server/config/rolesList.js
@@ -1,7 +1,6 @@
 const ROLES_LIST = {
   Admin: "Admin",
   BasicUser: "BasicUser",
-  ProUser: "ProUser",
 };
 
 module.exports = ROLES_LIST;
--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -33,8 +33,6 @@ const addRecipe = async (req, res, next) => {
       title,
       image,
       description,
-      calories,
-      cookingTime,
       ingredients,
       instructions,
     } = req.body;
@@ -42,8 +40,6 @@ const addRecipe = async (req, res, next) => {
       !title ||
       !image ||
       !description ||
-      !calories ||
-      !cookingTime ||
       !ingredients.length ||
       !instructions.length
     ) {
@@ -63,16 +59,12 @@ const updateRecipe = async (req, res, next) => {
       title,
       image,
       description,
-      calories,
-      cookingTime,
       ingredients,
       instructions,
     } = req.body;
     if (
       !title ||
       !image ||
-      !description ||
-      !calories ||
       !cookingTime ||
       !ingredients.length ||
       !instructions.length
@@ -90,9 +82,7 @@ const updateRecipe = async (req, res, next) => {
     foundRecipe.title = title;
     foundRecipe.description = description;
     foundRecipe.image = image;
-    foundRecipe.calories = calories;
     foundRecipe.ingredients = ingredients;
-    foundRecipe.cookingTime = cookingTime;
     foundRecipe.instructions = instructions;
 
     const updatedRecipe = await foundRecipe.save();
@@ -147,96 +137,6 @@ const deleteRecipe = async (req, res, next) => {
   }
 };
 
-const addComment = async (req, res, next) => {
-  try {
-    const { comment } = req.body;
-
-    // Validate userId and commentText
-    if (!comment) {
-      return res.status(400).json({ message: "Comment is required." });
-    }
-
-    const recipe = await Recipe.findById(req.params.id);
-    if (!recipe) {
-      return res.status(404).json({ message: "Recipe not found." });
-    }
-
-    // Add the new comment
-    recipe.comments.push({ user: req.user, comment });
-    await recipe.save();
-
-    res.status(201).json({ message: "Comment added successfully." });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const deleteComment = async (req, res, next) => {
-  try {
-    const { recipeId, commentId } = req.params;
-
-    const recipe = await Recipe.findById(recipeId);
-    if (!recipe) {
-      return res.status(404).json({ message: "Recipe not found." });
-    }
-
-    const commentIndex = recipe.comments.findIndex((comment) =>
-      comment._id.equals(commentId)
-    );
-    if (commentIndex === -1) {
-      return res.status(404).json({ message: "Comment not found." });
-    }
-
-    recipe.comments.splice(commentIndex, 1);
-    await recipe.save();
-
-    res.status(200).json({ message: "Comment deleted successfully." });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const toggleFavoriteRecipe = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const recipeIndex = user.favorites.indexOf(req.params.id);
-    if (recipeIndex === -1) {
-      // Recipe not present, add it to favorites
-      user.favorites.push(req.params.id);
-    } else {
-      // Recipe already present, remove it from favorites
-      user.favorites.splice(recipeIndex, 1);
-    }
-
-    await user.save();
-
-    const roles = Object.values(user.roles);
-
-    const accessToken = jwt.sign(
-      {
-        UserInfo: {
-          userId: user._id,
-          name: user.name,
-          email: user.email,
-          profilePicture: user.profilePicture,
-          roles: roles,
-          favorites: user.favorites,
-        },
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
-    return res.status(201).json({ accessToken });
-  } catch (error) {
-    next(error);
-  }
-};
-
 module.exports = {
   getAllRecipes,
   getRecipe,
@@ -244,7 +144,4 @@ module.exports = {
   updateRecipe,
   rateRecipe,
   deleteRecipe,
-  addComment,
-  deleteComment,
-  toggleFavoriteRecipe,
 };
